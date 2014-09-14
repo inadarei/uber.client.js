33cf34d3bf7643419a850efb08c903a0d33754f2
Irakli N <irakli@gmail.com>
2014-09-13 22:15:16 -0400

    init

--- test/message.js
+++ test/message.js
@@ -1,5 +1,18 @@
-var after  = require('after');
+var after  = require('after')
   , should = require('should')
   , assert = require('assert');
 
 var uc = require('../'); //uberclient
+
+
+describe('foo', function(){
+  it('should foo the bar out of you', function() {
+    assert.equal(1, 1);
+    //assert.equal(-1, [1,2,3].indexOf(0));
+  })
+})
+
+  it('should be glorious', function() {
+    assert.equal(1, 1);
+    //assert.equal(-1, [1,2,3].indexOf(0));
+  })
