1ada170847c2d10e4269198633e7bcc16cafd8b0
Irakli Nadareishvili <irakli@gmail.com>
2014-09-21 07:11:11 -0400

    querying works yaaaaay

--- test/dataTest.js
+++ test/dataTest.js
@@ -10,7 +10,6 @@ var Data = require('../lib/data').Data
 describe('Uber Data class', function() {
 
   describe('Constructor function', function() {
-
     var sampleJson;
 
     beforeEach(function(done){
@@ -30,13 +29,29 @@ describe('Uber Data class', function() {
         done();
     });
 
-    it.only('should parse data structure recursively producing data objects, not: just JSON', function(done) {
+    it('should parse deep data structure recursively producing data objects, not: just JSON', function(done) {
         var data  = new Data(sampleJson);
-        //console.dir(data.data[0]);
-
-        //console.log(data.rel);
-
+        data.data[0].url.should.equal('http://example.org/');
+        data.data[3].url.should.equal('http://example.org/list/1');
+        data.data[3].data.data[1].name.should.equal('dueDate');
+        data.data[3].data.data[1].value.should.equal('2014-05-01');
         done();
+    });
+
+    it.only('should be able to query through Data objects', function(done) {
+      var data  = new Data(sampleJson);
+      var result;
+
+      result = data.query({"name" : "todo"});
+      result[0].url.should.equal('http://example.org/list/1');
+
+      result = data.query({"id" : "search"});
+      result[0].model.should.equal('{&title}');
+
+      result = data.query({"rel" : "http://example.org/rels/todo"});
+      result[1].url.should.equal('http://example.org/list/2');
+
+      done();
     });
 
   });
